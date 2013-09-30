26f2ade71a7d4be7931345c0848b6f3f2c57f813
Aftha Jaldin <aftha.jaldin88@gmail.com>
2013-09-30 21:33:49 +0530

    commit-30-9-2013-2133

--- public/ajax_files/search_for_stops.php
+++ public/ajax_files/search_for_stops.php
@@ -45,24 +45,45 @@ for($i = 0; $i < count($routes_of_from_stop); $i++){
 			echo $route_object->find_by_id($routes_of_to_stop[$j]->route_id)->route_number . '<br />';
 			$flag = 1;
 			//return;
-		} else {
+		} /*else {
+			
 			$stops1 = $stop_route_object->get_stops_for_route($routes_of_from_stop[$i]->route_id);
 			$stops2 = $stop_route_object->get_stops_for_route($routes_of_to_stop[$j]->route_id);
+			
 			for ($k = 0; $k < count($stops1); $k++){
 				for ($l = 0; $l < count($stops2); $l++){
-					if ($stops1[$k]->stop_id == $stops2[$l]->stop_id){ // two bus
+					
+					if ($stops1[$k]->stop_id == $stops2[$l]->stop_id){
+						
 						echo 'Intersection Stop: <br />' . $stop_object->find_by_id($stops1[$k]->stop_id)->name . '<br /><br />';
 						
 						$intersection_stop_id = $stops1[$k]->stop_id;
 						
-						/** new */
-						echo 'Take ' . $route_object->find_by_id($routes_of_from_stop[$i]->route_id)->route_number . 
-						' to ' . $stop_object->find_by_id($intersection_stop_id)->name . ' then switch to ' .
-						$route_object->find_by_id($routes_of_to_stop[$j]->route_id)->route_number;
+						$routes_passing_through_intersection = $stop_route_object->get_routes_for_stop($intersection_stop_id);
+						
+						echo 'Routes passing through Intersection Stop: <br />';
+						
+						foreach ($routes_passing_through_intersection as $route){
+							echo $route_object->find_by_id($route->route_id)->route_number . '<br />';
+						}
+						
+						//return;
+						
+						/*
+						$routes_passing_through_from_stop_and_intersection = $stop_route_object->get_routes_for_stop($stops1[$k]->stop_id);
+						$routes_passing_through_to_stop_and_intersection = $stop_route_object->get_routes_for_stop($stops2[$l]->stop_id);
+						*/						
+						/*
+						echo 'k = '.$k;
+						echo '<br />';
+						echo 'l = '.$l;
+						echo '<br /><br /><br />';
+						*/
+						/*
 					}
 				}
 			}
-		}
+		}*/
 	}
 }
 
